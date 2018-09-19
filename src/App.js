8d8c80c5aef4139705ef42855afaf4d20164d0a2
Neword <newordn@gmail.com>
2018-09-19 05:11:56 +0100

    Adding routes(/login,/registration,/forget_password,/num_verification,/importation/finish,/importation/contact,/imporation/infos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
 import Header from './components/Header/headerAuth';
 import Footer from './components/Footer/footerAuth';
 import Login from './components/Login/Login';
+import './App.css';
+
 
 // the main spa page
 class App extends Component
@@ -10,12 +12,28 @@ class App extends Component
 	 render()
 	 {
 	 	return(
+			 <Router>
 		 		<div class="app">
-		 			<Header/>
-		 			<Login/>
+					  <Header />
+					<Switch>
+						{/*Account routes*/}
+		 				<Route path="/login" component={Login}></Route>
+						<Route path="/registration" component={}></Route>
+						<Route path="/forget_password" component={}></Route>
+						<Route path="/num_verification" component={}></Route>
+						{/*Account routes*/}
+
+						{/*Importation routes*/}
+						<Route path="/importation/finish" component={}></Route>
+						<Route path="/importation/contact" component={}></Route>
+						<Route path="/importation/infos" component={}></Route>
+						{/*Importation routes*/}
+						  <Route path="/" component={Login}></Route>
+					</Switch>
 		 			<Footer/>
 	 			</div>
-	 		);
+	 		</Router>
+			 );
 	 }
 }
 
